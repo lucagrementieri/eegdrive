1692f41a5f0aacc5d630524e3bdbe086cc1d175b
Luca Grementieri <lgrementieri@nextb.it>
2020-10-15 23:09:25 +0200

    :construction: Add transformations

--- eegdrive/ingestion/eeg.py
+++ eegdrive/ingestion/eeg.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Iterator
 
 import numpy as np
 
@@ -9,3 +10,19 @@ class EEG:
     preparation_state: np.ndarray
     action_state: np.ndarray
     frequency: float = 500.0
+
+    def slice(self, start: int, end: int) -> 'EEG':
+        return EEG(
+            self.data[:, start:end],
+            self.preparation_state[start:end],
+            self.action_state[start:end],
+            self.frequency,
+        )
+
+    def split_session(self) -> Iterator['EEG']:
+        start_indices = (
+                np.maximum(self.preparation_state[1:] - self.preparation_state[:-1], 0) + 1
+        )
+        end_indices = np.append(start_indices[1:], len(self.preparation_state))
+        for start, end in zip(start_indices, end_indices):
+            yield self.slice(start, end)
---
+++ eegdrive/ingestion/transforms.py
@@ -0,0 +1,61 @@
+import numpy as np
+from scipy import signal
+
+from .eeg import EEG
+
+
+class HighPass:
+    def __init__(self, threshold: float, order: int = 3):
+        self.threshold = threshold
+        self.order = order
+
+    def __call__(self, eeg: EEG) -> EEG:
+        nyq = 0.5 * eeg.frequency
+        high = self.threshold / nyq
+        # noinspection PyTupleAssignmentBalance
+        b, a = signal.butter(self.order, high, btype='highpass')
+        for c in eeg.data.shape[0]:
+            eeg.data[c] = signal.lfilter(b, a, eeg.data[c])
+        return eeg
+
+
+class RemoveBeginning:
+    def __init__(self, multiplier_order: int = 2):
+        self.multiplier = 10 ** multiplier_order
+
+    def __call__(self, eeg: EEG) -> EEG:
+        length = eeg.data.shape[1]
+        std = np.std(eeg.data[:, length // 10:], axis=1)
+        anomalous = np.any(np.abs(eeg.data) > self.multiplier * std, axis=0)
+        cutoff = len(anomalous) - anomalous[::-1].argmax()
+        next_preparation = np.argmax(eeg.preparation_state[cutoff:] > 0)
+        next_action = np.argmax(eeg.action_state[cutoff:] > 0)
+        if next_action < next_preparation:
+            previous_actions = eeg.action_state[:next_preparation] > 0
+            end_action = len(previous_actions) - np.argmax(previous_actions[::-1])
+            cutoff = (end_action + next_preparation) // 2
+        eeg.data = eeg.data[:, cutoff:]
+        eeg.preparation_state = eeg.preparation_state[cutoff:]
+        eeg.action_state = eeg.action_state[cutoff:]
+        return eeg
+
+
+class RemoveLineNoise:
+    def __init__(self, noise_frequency: float = 50.0, quality: float = 10.0):
+        self.noise_frequency = noise_frequency
+        self.quality = quality
+
+    def __call__(self, eeg: EEG) -> EEG:
+        nyq = 0.5 * eeg.frequency
+        for f in np.arange(self.noise_frequency, nyq, self.noise_frequency):
+            b, a = signal.iirnotch(f, self.quality, eeg.frequency)
+            for c in eeg.data.shape[0]:
+                eeg.data[c] = signal.lfilter(b, a, eeg.data[c])
+        return eeg
+
+
+class Standardize:
+    def __call__(self, eeg: EEG) -> EEG:
+        eeg.data -= eeg.data.mean(axis=1)
+        eeg.data /= eeg.data.std(axis=1)
+        return eeg
