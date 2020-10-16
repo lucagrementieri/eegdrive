223b956b8b69b115a9f61e45973ce2aae6b9587b
Luca Grementieri <lgrementieri@nextb.it>
2020-10-16 23:14:17 +0200

    :sparkles: Add RandomConv

---
+++ eegdrive/models/random_conv.py
@@ -0,0 +1,41 @@
+import itertools
+from typing import Tuple, List
+
+import torch
+from torch import nn
+
+
+class RandomConv1D(nn.Module):
+    def __init__(
+            self,
+            channels: int,
+            filters: int,
+            sizes: Tuple[int, ...],
+            max_dilation_exponent: int = 8,
+    ):
+        super().__init__()
+        self.convolutions = []
+        for k, d_exp, pad in itertools.product(
+                sizes, range(max_dilation_exponent + 1), (True, False)
+        ):
+            padding = (k - 1) // 2 if pad else 0
+            self.convolutions.append(
+                nn.Conv1d(
+                    channels,
+                    filters,
+                    k,
+                    padding=padding,
+                    dilation=2 ** d_exp,
+                    groups=channels,
+                )
+            )
+        self.random_weights()
+
+    def random_weights(self) -> None:
+        for conv in self.convolutions:
+            nn.init.normal_(conv.weight)
+            conv.weight -= conv.weight.mean(dim=-1, keepdim=False)
+            nn.init.uniform_(conv.bias, -1, 1)
+
+    def forward(self, x: torch.Tensor) -> List[torch.Tensor]:
+        return [conv(x) for conv in self.convolutions]
