137a1e3fdb0ced479473f05b2c64d271d6c25f9e
Luca Grementieri <lgrementieri@nextb.it>
2020-10-16 08:48:22 +0200

    :construction: Export session statistics

--- eegdrive/ingestion/ingest.py
+++ eegdrive/ingestion/ingest.py
@@ -1,25 +1,52 @@
 from pathlib import Path
+from typing import Union, Dict, List
 
-from .eeg import EEG
-from .transforms import HighPass, RemoveBeginning, RemoveLineNoise, Standardize
 import numpy as np
 from tqdm import tqdm
 
+from .eeg import EEG
+from .transforms import HighPass, RemoveBeginning, RemoveLineNoise, Standardize
 
-def ingest_session(data_path: Path, output_dir: Path):
+
+def ingest_session(
+        data_path: Path, output_dir: Path
+) -> Dict[str, Union[int, List[int]]]:
     eeg = EEG.from_hdf5(data_path)
     transforms = [HighPass(1.0), RemoveBeginning(), RemoveLineNoise(), Standardize()]
     for transform in transforms:
         eeg = transform(eeg)
+    statistics = {
+        'dataset_size': 0,
+        'action_count': [0] * 5,
+        'preparation_count': [0] * 5,
+        'essential_length_q': None,
+        'length_q': None,
+    }
+    lengths = []
+    essential_lengths = []
     for i, episode in enumerate(tqdm(eeg.split_session(), desc='Ingestion')):
         action_label = np.max(np.unique(episode.action_state))
         preparation_label = np.max(np.unique(episode.preparation_state))
         activity = episode.action_state + episode.preparation_state
-        min_length = activity.nonzero()[0][-1] + 1
+        essential_length = activity.nonzero()[0][-1] + 1
         np.savez(
             output_dir / f'{data_path.stem}_{i:04d}.npz',
             data=episode.data,
             action_label=action_label,
             preparation_label=preparation_label,
-            min_length=min_length,
+            essential_length=essential_length,
         )
+        lengths.append(episode.data.shape[1])
+        essential_lengths.append(essential_length)
+        statistics['dataset_size'] += 1
+        statistics['action_count'][action_label] += 1
+        statistics['preparation_count'][max(0, preparation_label - 4)] += 1
+    statistics['essential_length_q'] = np.quantile(
+        essential_lengths, (0, 0.25, 0.5, 0.75, 1)
+    )
+    statistics['essential_length_q'] = (
+        statistics['essential_length_q'].astype(np.int64).tolist()
+    )
+    statistics['length_q'] = np.quantile(lengths, (0, 0.25, 0.5, 0.75, 1))
+    statistics['length_q'] = statistics['length_q'].astype(np.int64).tolist()
+    return statistics
